8711a79aa72217d143b3ff5462728c25dcc19e2c
Aymen Bouhou <aymen.bouhou@gmail.com>
2022-10-15 10:32:29 +0200

    P

---
+++ Run.R
@@ -0,0 +1,4 @@
+library(rsconnect)
+rsconnect::setAccountInfo(name='aymenbouhou',token='8E52029A954F453D8ABEAEFB5EF89EB8',secret='/gtMidCppIzGvdaIyFIzHXQ5cgv4prgMEsn52Lac')
+library(shiny)
+runApp()
