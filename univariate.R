ef6a1972b81728324c37ce982257c44d092914d0
Aymen Bouhou <aymen.bouhou@gmail.com>
2022-10-18 13:51:30 +0200

    pipi

--- univariate.R
+++ univariate.R
@@ -1,47 +1,38 @@
 source("imports.R")
-# Affichage de la table statistiques pour les donees 
+# Affichage de la table statistiques pour les données 
+
 statQ <- function(var)
+{    
+    if (is.numeric(df[, var]))
+    {
+
+          temp2=data.frame(
+          stat = c('Min', 'Quantile 25%', 'Médiane', 'Quantile 75%', 'Max', 'Moyenne', 'Écart type'),
+          values = c(quantile(df[, var]), mean(df[, var]), sd(df[, var])))
+
+        return(setDT(temp2))
+
+    }
+    else
+    {      temp2 = as.data.frame(table(df[, var]))
+           temp2 = cbind(temp2, cumsum(temp2[[2]]))
+           temp2 = cbind(temp2,
+                       temp2[[2]]/nrow(df)*100,
+                       temp2[[3]]/nrow(df)*100)
+           colnames(temp2) <- c(var, "Effectifs", "Effectifs Cum.",
+                             "Fréquences", "Fréquences Cum.")
+
+        return(setDT(temp2))
+    }
+}
+
+
+plot_box_bar <- function(var)
 {
     
     if (is.numeric(df[, var]))
     {
-          tt=data.frame(
-          stat = c('Min', 'Quantile 25%', 'Médiane', 'Quantile 75%', 'Max', 'Moyenne', 'Écart type'),
-          values = c(quantile(df[, 'Temperature of patient']), mean(df[, 'Temperature of patient']), sd(df[, 'Temperature of patient']))
-                        )   
-        return(setDT(tt))
-    }
-    else
-    {      tt = as.data.frame(table(df[, var]))
-           tt = cbind(tt, cumsum(tt[[2]]))
-           tt = cbind(tt,
-                       tt[[2]]/nrow(df)*100,
-                       tt[[3]]/nrow(df)*100)
-           colnames(tt) <- c(var, "Effectifs", "Effectifs Cum.",
-                             "Fréquences", "Fréquences Cum.")
-
-<<<<<<< HEAD
-        return(setDT(tt))
-    }
-}
-=======
-# Affichage de la table statistiques pour les donees quantitatives
-quantitativeTabStats <- reactive({
-  data.frame(
-      stat = c('Min', 'Quantile 25%', 'Médiane', 'Quantile 75%', 'Max', 'Moyenne', 'Écart type'),
-      values = c(quantile(df[, input$univariateSelect]), mean(df[, input$univariateSelect]), sd(df[, input$univariateSelect]))
-  )
-})
-
-
->>>>>>> 2fd08b1a6bb1e49f61c4500d12adeede2700c5ce
-
-plot_box_bar <- function(var)
-{
-	
-    if (is.numeric(df[, var]))
-	{
-	    df1=df %>% select(var)
+        df1=df %>% select(var)
         colnames(df1)[1] = "col"
             fig <- ggplot(df1, aes(x="", y=col)) + 
                 geom_boxplot()+
@@ -49,23 +40,24 @@ plot_box_bar <- function(var)
                 ggtitle(paste('Boîte à moustaches de ', var))+
                 ggeasy::easy_center_title()
         
-	    
-	    return(fig)
-	}
-	else
-	{
+        
+        return(fig)
+    }
+    else
+    {
         
         temp1 <- as.data.frame(table(df[, var]))
         colnames(temp1)[1] = "Var1"
-	       fig <- ggplot(temp1, aes(x=Var1, y=Freq, fill=Var1))+
+           fig <- ggplot(temp1, aes(x=Var1, y=Freq, fill=Var1))+
                 geom_bar(stat="identity") +
+                theme(legend.title=element_blank())+
                 scale_fill_brewer(palette="Blues")+
                 xlab(var) + ylab("Fréquence")+
                 ggtitle(paste('Diagramme en clonnes de ', var))+
                 ggeasy::easy_center_title()
     
-	    return(fig)
-	}
+        return(fig)
+    }
 }
 
 
@@ -89,9 +81,10 @@ plot_histo_pie <- function(var)
         colnames(temp1)[1] = "Var1"
         fig <-  ggplot(temp1, aes(x="", y=Freq, fill=Var1)) +
                 geom_bar(stat="identity", width=1) +
+                theme(legend.title=element_blank())+
                 coord_polar("y", start=0)+
                 guides(fill=guide_legend(title=var))+
-                xlab("") + ylab("")
+                xlab(var) + ylab("")
 
         return(fig)
     }
